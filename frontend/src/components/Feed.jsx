f57c3c04393c746c42992fc3cc63514a9c60f79d
ishan-gond-ui <ishangond05@gmail.com>
2024-12-17 17:45:31 +0530

    Update Feed.jsx

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Posts from './Posts'
+import React from 'react';
+import Posts from './Posts';
 
 const Feed = () => {
   return (
-    <div className='flex-1 my-8 flex flex-col items-center pl-[20%]'>
-        <Posts/>
+    <div className="flex justify-center items-start my-8 px-4 w-full">
+      <div className="flex flex-col items-center w-full max-w-2xl">
+        <Posts />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
