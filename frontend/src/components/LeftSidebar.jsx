284ac6f2a7921217d661c5a166bf9e8795f3afb1
ishan-gond-ui <ishangond05@gmail.com>
2024-12-20 09:56:14 +0530

    Update LeftSidebar.jsx

--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -20,7 +20,7 @@ const LeftSidebar = () => {
 
   const logoutHandler = async () => {
     try {
-      const res = await axios.get('http://localhost:8000/api/v1/user/logout', { withCredentials: true });
+      const res = await axios.get('https://edu-gathering.onrender.comapi/v1/user/logout', { withCredentials: true });
       if (res.data.success) {
         dispatch(setAuthUser(null));
         dispatch(setSelectedPost(null));
