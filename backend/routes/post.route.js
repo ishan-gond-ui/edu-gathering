e78b8952fd7552a36984e4f9ecbc2c0c11a4e764
ishan-gond-ui <ishangond05@gmail.com>
2024-12-20 11:45:12 +0530

    Update post.route.js

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,12 +1,23 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import upload from "../middlewares/multer.js";
-import { addComment, addNewPost, bookmarkPost, deletePost, dislikePost, getAllPost, getCommentsOfPost, getUserPost, likePost } from "../controllers/post.controller.js";
+import {
+    addComment, 
+    addNewPost, 
+    bookmarkPost, 
+    deletePost, 
+    dislikePost, 
+    getAllPost, 
+    getCommentsOfPost, 
+    getUserPost, 
+    likePost 
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.route("/addpost").post(isAuthenticated, upload.single('image'), addNewPost);
-router.route("/all").get(isAuthenticated,getAllPost);
+// Updated field name for Multer to match the frontend field name 'media'
+router.route("/addpost").post(isAuthenticated, upload.single('media'), addNewPost);
+router.route("/all").get(isAuthenticated, getAllPost);
 router.route("/userpost/all").get(isAuthenticated, getUserPost);
 router.route("/:id/like").get(isAuthenticated, likePost);
 router.route("/:id/dislike").get(isAuthenticated, dislikePost);
@@ -16,4 +27,3 @@ router.route("/delete/:id").delete(isAuthenticated, deletePost);
 router.route("/:id/bookmark").get(isAuthenticated, bookmarkPost);
 
 export default router;
-
