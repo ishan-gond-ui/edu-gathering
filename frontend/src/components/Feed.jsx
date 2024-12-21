202ce5179a145788b2f19a538f7a7b1f80eeb5c9
ishan-gond-ui <ishangond05@gmail.com>
2024-12-21 13:24:59 +0530

    Update Feed.jsx

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Posts from './Posts';
+import StoryComponent from './StoryComponent';  // Import the StoryComponent
 
 const Feed = () => {
   const { posts } = useSelector((store) => store.post); // Fetch posts from Redux
@@ -11,6 +12,10 @@ const Feed = () => {
   return (
     <div className="flex justify-center items-start my-8 px-4 w-full">
       <div className="flex flex-col items-center w-full max-w-2xl">
+        {/* Add StoryComponent at the top */}
+        <StoryComponent />  {/* Instagram-like Stories section */}
+
+        {/* Render posts if available */}
         {posts.length > 0 ? (
           <Posts posts={posts} />
         ) : (
